4e0296a89397f3fbd46ba5fa68eea453d7846f72
unknown <snow28@ukr.net>
2018-02-06 23:14:53 +0100

    pure_js

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,25 @@
-$(document).ready(function(){
+function moveLeft(){
+    //document.getElementsByClassName('aside-left').addClass('aside-left-active');
 
+    var element = document.getElementById("aside-left");
+    element.classList.add("aside-left-active");
+}
+
+function moveRight(){
+    var element2 = document.getElementById("aside-right");
+    element2.classList.add("aside-right-active");
+}
+
+$(document).ready(function(){
+    //document.getElementsByClassName('header__b-left-js').
 
     //   always visible div JS
     var navOffsetY = $('.main__fluid-elem').offset().top;
     var navOffsetX = $('.main__fluid-elem').offset().left;
 
     $(window).scroll(function () {
-        var scrollPos = $(window).scrollTop();
-        if(scrollPos >= navOffsetY){
+        var scrollPosTop = $(window).scrollTop();
+        if(scrollPosTop >= navOffsetY-40){
             $('.main__fluid-elem').addClass('fixed');
             $('.main__fluid-elem').css('left', navOffsetX);
         }else{
@@ -15,12 +27,12 @@ $(document).ready(function(){
         }
     })
 
-    $('.header__b-left-js').on('click', function(){
+   /*$('.header__b-left-js').on('click', function(){
         $('.aside-left').toggleClass('aside-left-active');
     });
     $('.header__b-right-js').on('click', function(){
         $('.aside-right').toggleClass('aside-right-active');
-    });
+    });*/
 
 
 });
